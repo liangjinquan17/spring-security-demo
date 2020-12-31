0fbc94ff2532ddf2a24292a48953a6b013b9f5a2
liangjinquan <liangjinquan17@gmail.com>
2020-12-31 17:13:44 +0800

    feat:初始化spring security

---
+++ src/test/java/com/sf/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
